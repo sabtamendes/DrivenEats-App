3176e9c41818f0ef7e19cdc1ea6facae4abcbbd8
Sabta Mendes <sabtaamendes@gmail.com>
2022-08-10 16:01:23 -0400

    função selecionado da seção de bebidas completo

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function foodCard(element, foodName, value) {
     const tag = document.querySelector(".main-food .card-border .check");
     tag.classList.remove("hidden");
 }
+
+function drinkCard(element, drinkName, value){
+    const borderGreen = document.querySelector(".main-drink .card-border");
+    const check = document.querySelector(".main-drink .card-border .check");
+    if(borderGreen !== null){
+        borderGreen.classList.remove("card-border");
+        check.classList.add("hidden");
+    }
+    element.classList.add("card-border");
+    const tag = document.querySelector(".main-drink .card-border .check");
+    tag.classList.remove("hidden");
+}
